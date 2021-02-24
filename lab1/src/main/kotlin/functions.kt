1898632fab476e2b44349c521405ae356ae23c4b
Avvessalom <avvessalom@gmail.com>
2021-02-24 19:03:06 +0300

    add sin func

--- lab1/src/main/kotlin/functions.kt
+++ lab1/src/main/kotlin/functions.kt
@@ -1,8 +1,13 @@
 import kotlin.math.*
-fun axaxa(): String{
-    return "axaxa"
-}
 
-fun main(){
-    println(axaxa())
+fun mySin(x: Number): Double {
+    val fact: (Int) -> Int = { (1..it).fold(1) {acc, i -> acc * i} }
+    return generateSequence(0){ n -> n + 1}
+        .take(17)
+        .map{n -> ((-1.0).pow(n) * x.toDouble().pow(2 * n + 1)) / fact(2 * n + 1)}
+        .sum()
+
+}
+fun main() {
+    println(mySin(0.1))
 }
