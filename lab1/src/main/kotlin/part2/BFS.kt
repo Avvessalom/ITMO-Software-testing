fa134d7cf58c0dd2071616476787ab04a27b27f3
Vitaliy <mr.prikota@gmail.com>
2021-02-25 11:56:05 +0300

    Added part2

---
+++ lab1/src/main/kotlin/part2/BFS.kt
@@ -0,0 +1,72 @@
+package part2
+
+import java.util.*
+import kotlin.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashSet
+
+fun search(start: Int, finish: Int, graphMatrix: ArrayList<IntArray>): ArrayList<IntArray> {
+    val visited = HashSet<Int>()
+    val searchQueue = ArrayDeque<Int>()
+
+    visited.add(start)
+    searchQueue.addLast(start)
+
+    val marked = ArrayList<Int>()
+    val graph = ArrayList<IntArray>()
+    var currentNode: Int
+    while (searchQueue.size > 0) {
+        currentNode = searchQueue.pop()
+        visited.add(currentNode)
+        if (currentNode == finish) {
+            return graph
+        }
+
+        for (i in graphMatrix[currentNode]) {
+            if (!visited.contains(i)) {
+                if (!marked.contains(i)) {
+                    marked.add(i)
+                    graph.add(intArrayOf(currentNode, i))
+                }
+                if (i == finish) {
+                    return graph
+                }
+                searchQueue.addLast(i)
+            }
+        }
+    }
+
+    return graph
+}
+
+fun main() {
+    val array = ArrayList<IntArray>()
+    array.add(intArrayOf(1, 2, 3))
+    array.add(intArrayOf(2, 5, 6))
+    array.add(intArrayOf(1, 5, 6))
+    array.add(intArrayOf())
+    array.add(intArrayOf(2))
+    array.add(intArrayOf())
+    array.add(intArrayOf())
+    array.add(intArrayOf(5, 6))
+
+    val start = 0;
+    val finish = 7;
+    val graph = search(start, finish, array)
+    var found = false;
+    for (i in graph) {
+        print(i[0])
+        print("->")
+        print(i[1])
+        if (i[1] == finish) {
+            found = true;
+        }
+        println()
+    }
+    if (found) {
+        println("Path found!")
+    }
+    else {
+        println("Path wasn't found!")
+    }
+}
