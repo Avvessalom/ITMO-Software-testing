cf125167a0f231c6cfb0b0c69db9e57ad609b67f
Avvessalom <49491863+Avvessalom@users.noreply.github.com>
2021-02-25 11:30:19 +0300

    finish tests for sin()

--- lab1/src/main/kotlin/functions.kt
+++ lab1/src/main/kotlin/functions.kt
@@ -3,7 +3,7 @@ import kotlin.math.*
 fun mySin(x: Number): Double {
     val fact: (Int) -> Int = { (1..it).fold(1) {acc, i -> acc * i} }
     return generateSequence(0){ n -> n + 1}
-        .take(8)
+        .take(7)
         .map{n -> ((-1.0).pow(n) * x.toDouble().pow(2 * n + 1)) / fact(2 * n + 1)}
         .sum()
 }
@@ -11,9 +11,4 @@ fun mySin(x: Number): Double {
 fun stupidSin(x: Number): Double{
     val fact: (Int) -> Int = { (1..it).fold(1) {acc, i -> acc * i} }
     return (x.toDouble() - x.toDouble().pow(3) / fact(3) + x.toDouble()/ fact(5))
-}
-
-fun main() {
-    println(mySin(2 * PI / 3))
-    println(stupidSin(2 * PI / 3))
 }
--- lab1/src/test/kotlin/functions.kt
+++ lab1/src/test/kotlin/functions.kt
@@ -10,24 +10,36 @@ class FunctionsTests {
     @Test fun `mySin(x) algorithm test` () = assertAll(
         { assertEquals(0.09983341664682815, mySin(0.1), 0.001) },
         { assertEquals(0.8414709848079 , mySin(1), 0.001)},
+    )
 
-        { assertEquals(1.0, mySin(PI / 2), 0.001) },
+    @Test fun `mySin(x) boundary points test` () = assertAll(
         { assertEquals(0.0 , mySin(0), 0.001) },
+        { assertEquals(1.0, mySin(PI / 2), 0.001) },
+        { assertEquals(0.0, mySin(PI), 0.01) },
+        { assertEquals(-1.0, mySin(-1.5707963267948966), 0.001)}
+    )
 
-        )
-    @Test fun `mySin(x) first quarter` () = assertAll(
+    @Test fun `mySin(x) first quarter test` () = assertAll(
         { assertEquals(0.5, mySin(PI / 6), 0.001) },
         { assertEquals(sqrt(2.0) / 2, mySin(PI / 4), 0.001) },
         { assertEquals(sqrt(3.0) / 2, mySin(PI / 3), 0.001) },
         )
 
-    @Test fun `mySin(x) second quarter` () = assertAll(
+    @Test fun `mySin(x) second quarter test` () = assertAll(
         { assertEquals(sqrt(3.0) / 2, mySin(2 * PI / 3), 0.001) },
         { assertEquals(sqrt(2.0) / 2, mySin(3 * PI / 4), 0.001) },
         { assertEquals(0.5, mySin(5 * PI / 6), 0.001)}
     )
 
-    @Test fun `mySin(x) third quarter` () = assertAll(
+    @Test fun `mySin(x) third quarter test` () = assertAll(
+        { assertEquals(-0.5, mySin(7 * PI / 6), 0.01) },
+        { assertEquals(-1 * sqrt(2.0) / 2, mySin(5 * PI / 4), 0.1)},
+        { assertEquals(-sqrt(3.0) / 2, mySin(4 * PI / 3), 0.1)}
+    )
 
+    @Test fun `mySin(x) forth quarter test` () = assertAll(
+        { assertEquals(-1 * sqrt(3.0) / 2, mySin(-1.04719), 0.001)},
+        { assertEquals(-sqrt(2.0) / 2, mySin(-0.7853981633974486), 0.001)},
+        { assertEquals(-0.5, mySin(-0.523599), 0.001)}
     )
 }
