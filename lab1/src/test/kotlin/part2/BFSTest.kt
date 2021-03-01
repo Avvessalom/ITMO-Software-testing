e6997f364a57336655db52978929c987fb6df4ae
Vitaliy <mr.prikota@gmail.com>
2021-03-01 13:40:07 +0300

    Added initializing of large graph

--- lab1/src/test/kotlin/part2/BFSTest.kt
+++ lab1/src/test/kotlin/part2/BFSTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.*
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BFSTest {
     val smallGraph = ArrayList<IntArray>()
+    val largeGraph = ArrayList<IntArray>()
 
     @BeforeAll
     fun initSmallGraph() {
@@ -31,5 +32,27 @@ class BFSTest {
     @Test
     fun `test graph1 with path 0--0`() {
         Assertions.assertTrue(isReachable(0, 0, smallGraph))
+    }
+
+    @BeforeAll
+    fun initLargeGraph() {
+        largeGraph.add(intArrayOf(2, 7))
+        largeGraph.add(intArrayOf(0, 2, 4, 5))
+        largeGraph.add(intArrayOf(1))
+        largeGraph.add(intArrayOf(6, 17))
+        largeGraph.add(intArrayOf(5))
+        largeGraph.add(intArrayOf(1, 9))
+        largeGraph.add(intArrayOf(3, 5))
+        largeGraph.add(intArrayOf())
+        largeGraph.add(intArrayOf(4, 7, 10, 12))
+        largeGraph.add(intArrayOf(10))
+        largeGraph.add(intArrayOf(9, 13, 17))
+        largeGraph.add(intArrayOf(8, 14, 15))
+        largeGraph.add(intArrayOf(8, 9, 13, 15))
+        largeGraph.add(intArrayOf(12))
+        largeGraph.add(intArrayOf(11, 15))
+        largeGraph.add(intArrayOf(16, 17))
+        largeGraph.add(intArrayOf(17))
+        largeGraph.add(intArrayOf(13))
     }
 }
