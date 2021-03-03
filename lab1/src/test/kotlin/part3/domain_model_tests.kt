2f8cb64907f9ac437e014510fb8f836a46e9029c
Avvessalom <49491863+Avvessalom@users.noreply.github.com>
2021-03-03 23:24:22 +0300

    add third part of tests (domain model tests)

--- lab1/src/test/kotlin/part3/domain_model_tests.kt
+++ lab1/src/test/kotlin/part3/domain_model_tests.kt
@@ -1,8 +1,6 @@
 package part3
 
 import org.junit.jupiter.api.*
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
@@ -10,20 +8,55 @@ import kotlin.test.assertTrue
 
 class domain_model_tests {
     private val testHuman = Human("test1", 10)
-    private val testHuman1 = Human("Триллиан", 10)
+    private val trilian = Human("Триллиан", 10)
     private val testRobot = Robot("kek")
+    private val testPlase = Place("угол")
+    private val testLeg = LegOfRobot("test")
+    private val testHead = HeadOfRobot("test")
+
     @Test
-    fun `human is alien`(){
-        assertTrue {testHuman.seem(testHuman.isAlienWatcher)}
+    fun `human is alien`() {
+        assertTrue { testHuman.seem(testHuman.isAlienWatcher) }
     }
 
     @Test
-    fun `trillian is alien`(){
-        assertTrue {testHuman1.seem(testHuman1.isAlienWatcher)}
+    fun `trillian is alien`() {
+        assertTrue { trilian.seem(trilian.isAlienWatcher) }
     }
 
     @Test
-    fun `test stop robot`(){
-        assertEquals("Остановился перед Триллиан.", testRobot.stop(testHuman1))
+    fun `test stop robot`() {
+        assertEquals("Остановился перед ${trilian.toString()}.", testRobot.stop(trilian))
+    }
+
+    @Test
+    fun `test robot sit`() {
+        assertEquals("сидит в ${testPlase.toString()}. ", testRobot.sit(testPlase))
+    }
+
+    @Test
+    fun `test robot getup`() {
+        assertEquals("жоска поднялся из ${testPlase.toString()} на ${testLeg.toString()}.",
+            testRobot.getUp(testPlase, "жоска", testLeg))
+    }
+
+    @Test
+    fun `test try to go`() {
+        assertEquals(" героическая попытка пересечь ${testPlase.toString()}.", testRobot.tryToGo(testPlase))
+    }
+
+    @Test
+    fun `test watch`() {
+        assertEquals("Смотреть сквозь левое плечо ${testHuman.toString()}", testRobot.watch(testHuman))
+    }
+
+    @Test
+    fun `test robot name`() {
+        assertEquals("Робот ", testRobot.toString())
+    }
+
+    @Test
+    fun `test robot head action`() {
+        assertEquals("Голова робота резко дернулась вверх. ", testHead.action("резко", "дернулась вверх"))
     }
 }
