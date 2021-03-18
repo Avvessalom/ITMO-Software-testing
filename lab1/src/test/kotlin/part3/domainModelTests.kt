c4d1f38582566a6263a4e71ac4b71d909f5aca63
Avvessalom <49491863+Avvessalom@users.noreply.github.com>
2021-03-18 10:58:40 +0300

    update domain model tests

--- lab1/src/test/kotlin/part3/domainModelTests.kt
+++ lab1/src/test/kotlin/part3/domainModelTests.kt
@@ -2,6 +2,8 @@ package part3
 
 import org.junit.jupiter.api.*
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotEquals
 import kotlin.test.assertTrue
 
 
@@ -11,71 +13,103 @@ class DomainModelTests {
     lateinit var testRobot: Robot
     lateinit var trilian: Human
     lateinit var testPlase: Place
+    lateinit var testPlase1: Place
     lateinit var testLeg: LegOfRobot
     lateinit var testHead: HeadOfRobot
 
     @BeforeEach
-    fun init(){
+    fun init() {
         testHuman = Human("testHuman", 10)
         testRobot = Robot("testRobot")
         trilian = Human("Триллиан", 10)
         testPlase = Place("угол")
+        testPlase1 = Place("test")
         testLeg = LegOfRobot("test")
         testHead = HeadOfRobot("test")
     }
 
     @Test
     @DisplayName("Human is alien")
-    fun `human is alien`() {
-        assertTrue { testHuman.seem(testHuman.isAlienWatcher) }
-    }
+    fun `human is alien`() = assertAll(
+        {
+            assertTrue { testHuman.seem(testHuman.isAlienWatcher) }
+        },
+        {
+            testHuman.isAlienWatcher = true
+            assertTrue { testHuman.seem(testHuman.isAlienWatcher) }
+        },
+        {
+            testHuman.isAlienWatcher = false
+            assertTrue { testHuman.seem(testHuman.isAlienWatcher) }
+        })
 
     @Test
     @DisplayName("Trillian is alien")
-    fun `trillian is alien`() {
-        assertTrue { trilian.seem(trilian.isAlienWatcher) }
-    }
+    fun `trillian is alien`() = assertAll(
+        {
+            assertTrue { trilian.seem(trilian.isAlienWatcher) }
+        },
+
+        {
+            trilian.isAlienWatcher = true
+            assertTrue { trilian.seem(trilian.isAlienWatcher) }
+        },
+
+        {
+            trilian.isAlienWatcher = false
+            assertFalse { trilian.seem(trilian.isAlienWatcher) }
+        }
+    )
 
     @Test
     @DisplayName("stop robot")
-    fun `test stop robot`() {
-        assertEquals("Остановился перед ${trilian.toString()}.", testRobot.stop(trilian))
-    }
+    fun `test stop robot`() = assertAll(
+        { assertEquals("Остановился перед ${trilian.toString()}.", testRobot.stop(trilian)) },
+        { assertEquals("Остановился перед ${testHuman.toString()}.", testRobot.stop(testHuman))}
+    )
 
     @Test
     @DisplayName("robot sit")
-    fun `test robot sit`() {
-        assertEquals("сидит в ${testPlase.toString()}. ", testRobot.sit(testPlase))
-    }
+    fun `test robot sit`() = assertAll(
+        { assertEquals("сидит в ${testPlase.toString()}. ", testRobot.sit(testPlase)) },
+        { assertEquals("сидит в ${testPlase1.toString()}. ", testRobot.sit(testPlase1))}
+    )
 
     @Test
     @DisplayName("robot getup")
-    fun `test robot getup`() {
-        assertEquals("жоска поднялся из ${testPlase.toString()} на ${testLeg.toString()}.",
-            testRobot.getUp(testPlase, "жоска", testLeg))
-    }
+    fun `test robot getup`() = assertAll(
+        { assertEquals("жоска поднялся из ${testPlase.toString()} на ${testLeg.toString()}.",
+            testRobot.getUp(testPlase, "жоска", testLeg)) },
+        { assertEquals("тест поднялся из ${testPlase1.toString()} на ${testLeg.toString()}.",
+            testRobot.getUp(testPlase1, "тест", testLeg))}
+    )
 
     @Test
     @DisplayName("robot try to go")
-    fun `test try to go`() {
-        assertEquals(" героическая попытка пересечь ${testPlase.toString()}.", testRobot.tryToGo(testPlase))
-    }
+    fun `test try to go`() = assertAll(
+        { assertEquals(" героическая попытка пересечь ${testPlase.toString()}.", testRobot.tryToGo(testPlase)) },
+        { assertEquals(" героическая попытка пересечь ${testPlase1.toString()}.", testRobot.tryToGo(testPlase1))}
+    )
 
     @Test
     @DisplayName("robot watch")
-    fun `test watch`() {
-        assertEquals("Смотреть сквозь левое плечо ${testHuman.toString()}", testRobot.watch(testHuman))
-    }
+    fun `test watch`() = assertAll(
+        { assertEquals("Смотреть сквозь левое плечо ${testHuman.toString()}", testRobot.watch(testHuman)) },
+        { assertEquals("Смотреть сквозь левое плечо ${trilian.toString()}", testRobot.watch(trilian)) }
+    )
 
     @Test
     @DisplayName("robot name")
-    fun `test robot name`() {
-        assertEquals("Робот ", testRobot.toString())
-    }
+    fun `test robot name`() = assertAll(
+        { assertEquals("Робот ", testRobot.toString()) },
+        { assertNotEquals("Виталя", testRobot.toString()) }
+    )
 
     @Test
     @DisplayName("robot head action")
-    fun `test robot head action`() {
-        assertEquals("Голова робота резко дернулась вверх. ", testHead.action("резко", "дернулась вверх"))
-    }
+    fun `test robot head action`() = assertAll(
+        { assertEquals("Голова робота спокойно не двигалась. ", testHead.action("спокойно", "не двигалась")) },
+        { assertEquals("Голова робота резко дернулась вверх. ", testHead.action("резко", "дернулась вверх")) },
+        { assertEquals("Голова робота внезапно ринулась в пляс. ", testHead.action("внезапно", "ринулась в пляс")) }
+    )
 }
