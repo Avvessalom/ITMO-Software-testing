945948aeb58b80fc9c587650998ff1d1ef2b0fa9
avvessalom <avvessalom@gmail.com>
2021-03-22 16:19:46 +0300

    add log func

--- lab2/src/main/kotlin/logarithmic/Log.kt
+++ lab2/src/main/kotlin/logarithmic/Log.kt
@@ -1,7 +1,13 @@
 package logarithmic
 
 class Log: LogarithmicFunction<Number> {
+
+    var ln = Ln()
+
     override fun invoke(vararg args: Number, precision: Double): Double {
-        TODO("Not yet implemented")
+        if (args.size < 2) throw IllegalArgumentException("Two arguments!!! Parameter and Base")
+        val (x, base) = args.map { it.toDouble() }
+        return if (x <= 0 || x.isSpecial) Double.NaN else
+            ln(x, precision=precision) / ln(base, precision=precision)
     }
 }
