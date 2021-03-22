5d2d43949843fa5072b391d6d81c85ef0c616cfc
avvessalom <avvessalom@gmail.com>
2021-03-22 14:54:05 +0300

    update cos

--- lab2/src/main/kotlin/trigonometric/Cos.kt
+++ lab2/src/main/kotlin/trigonometric/Cos.kt
@@ -1,7 +1,12 @@
 package trigonometric
 
+import kotlin.math.*
+
 class Cos: TrigonometricFunction<Number> {
-    override fun invoke(vararg args: Number, presition: Double): Double {
-        TODO("Not yet implemented")
+    override fun invoke(vararg args: Number): Double {
+        val x = args[0].toDouble()
+        val sin = Sin()
+        return if (x.isSpecial) x else
+            sin(x + PI / 2)
     }
 }
