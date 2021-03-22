d9fb0afb15724cdc22f87a1cb6992dcb3e6bdb57
avvessalom <avvessalom@gmail.com>
2021-03-22 17:31:24 +0300

    add main math function

---
+++ lab2/src/main/kotlin/mainFunction/MainFunction.kt
@@ -0,0 +1,24 @@
+package mainFunction
+
+import logarithmic.*
+import trigonometric.*
+import kotlin.math.pow
+
+class MainFunction {
+    var sin = Sin()
+    var cos = Cos()
+    var tan = Tan()
+    var cot = Cot()
+    var csc = Csc()
+    var ln = Ln()
+    var log = Log()
+
+    fun invoke(vararg args: Number, precision: Double): Double{
+        val x = args[0].toDouble()
+        val p = precision
+        return if (x <=  0)
+            ((tan(x) * cos(x)) - (csc(x) - cos(x)).pow(2) / cot(x)).pow(2)
+        else
+            (((( (log(x, 3) + log(x, 2)) / (log(x, 5).pow(3)) ) + log(x, 10)) + log(x, 10)) / (log(x, 5) - log(x, 5)))
+    }
+}
