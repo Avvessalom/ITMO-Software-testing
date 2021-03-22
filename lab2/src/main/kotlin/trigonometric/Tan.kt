3569ff1f43845764fbb910eea0004ef3878c18df
avvessalom <avvessalom@gmail.com>
2021-03-22 15:26:08 +0300

    add Tan

--- lab2/src/main/kotlin/trigonometric/Tan.kt
+++ lab2/src/main/kotlin/trigonometric/Tan.kt
@@ -1,7 +1,15 @@
 package trigonometric
 
 class Tan: TrigonometricFunction<Number> {
-    override fun invoke(vararg args: Number, presition: Double): Double {
-        TODO("Not yet implemented")
+
+    val sin = Sin()
+    val cos = Cos()
+
+    override fun invoke(vararg args: Number): Double {
+
+        val x = args[0].toDouble()
+
+        return if (x.isSpecial) x else
+            sin(x) / cos (x)
     }
 }
