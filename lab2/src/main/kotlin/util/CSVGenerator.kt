269224567ce6b9c213023fb47f69c1c30e74fd4d
Vitaliy <mr.prikota@gmail.com>
2021-03-27 19:46:27 +0300

    Added csv generator

---
+++ lab2/src/main/kotlin/util/CSVGenerator.kt
@@ -0,0 +1,46 @@
+package util
+
+import logarithmic.LogarithmicFunction
+import trigonometric.TrigonometricFunction
+import java.io.FileOutputStream
+import java.io.PrintStream
+import java.nio.file.Files.createDirectories
+import java.nio.file.Path
+import java.util.*
+
+class CSVGenerator(_filepath: String = "src/test/resources/results/") {
+    private var filepath = _filepath
+    private var filename = "result.csv"
+    fun setFilePath(filename: String) {
+        this.filename = filename
+    }
+
+    fun generate(func: LogarithmicFunction<Number>, step: Double,
+                 lowerBound: Double, upperBound: Double, precision: Double, base: Double) {
+        createDirectories(Path.of(filepath))
+        val printStream = PrintStream(FileOutputStream(filepath + filename, true))
+        printStream.use {
+            var x = lowerBound
+            while (x < upperBound) {
+                val result = func(x, base, precision)
+                val csvString = String.format(Locale.US, "%f, %f", x, result)
+                printStream.println(csvString)
+                x += step
+            }
+        }
+    }
+
+    fun generate(func: TrigonometricFunction<Number>, step: Double, lowerBound: Double, upperBound: Double) {
+        createDirectories(Path.of(filepath))
+        val printStream = PrintStream(FileOutputStream(filepath + filename, true))
+        printStream.use {
+            var x = lowerBound
+            while (x < upperBound) {
+                val result = func(x)
+                val csvString = String.format(Locale.US, "%f, %f", x, result)
+                printStream.println(csvString)
+                x += step
+            }
+        }
+    }
+}
---
+++ lab2/src/test/kotlin/util/CSVGeneratorTest.kt
@@ -0,0 +1,24 @@
+package util
+
+import logarithmic.Log
+import org.junit.jupiter.api.Test
+import trigonometric.Sin
+import java.lang.Math.PI
+
+class CSVGeneratorTest {
+    private val generator = CSVGenerator()
+
+    @Test
+    fun testLog5() {
+        val log = Log()
+        generator.setFilePath("test-log5-0.1-0.1-1-result.csv")
+        generator.generate(log, 0.1, 0.1, 1.0, 0.1, 5.0)
+    }
+
+    @Test
+    fun testSin() {
+        val sin = Sin()
+        generator.setFilePath("test-sin-0.1-0-2pi-result.csv")
+        generator.generate(sin, 0.1, 0.0, 2*PI)
+    }
+}
