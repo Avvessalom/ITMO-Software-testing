7aca95ef412e63e92ade95ca0550de9f72fafafa
avvessalom <avvessalom@gmail.com>
2021-04-01 02:11:38 +0300

    log func, but with 1 little bug

--- lab2/src/test/kotlin/mainFunction/MainIntegrationTests.kt
+++ lab2/src/test/kotlin/mainFunction/MainIntegrationTests.kt
@@ -16,8 +16,9 @@ class MainIntegrationTests {
     val cotMock = Mockito.mock(Cot::class.java)
     val cscMock = Mockito.mock(Csc::class.java)
     val tanMock = Mockito.mock(Tan::class.java)
-    val lnMock = Mockito.mock(Ln::class.java)
-    val logMock = Mockito.mock(Log::class.java)
+    val log3Mock = Mockito.mock(Log::class.java)
+    val log5Mock = Mockito.mock(Log::class.java)
+    val log10Mock = Mockito.mock(Log::class.java)
     var sin = Sin()
     var cos = Cos()
     var tan = Tan()
@@ -84,5 +85,41 @@ class MainIntegrationTests {
                 0.01
             )
         }
+    }
+
+    @DisplayName("main test log3")
+    @ParameterizedTest
+    @CsvFileSource(resources = ["/results/test-log3-0.1-0.1-1-result.csv"])
+    fun `main test log3`(value: Double, answer: Double){
+        val main = MainFunction().apply { log = log3Mock }
+        Mockito.`when`(log3Mock(value)).thenReturn(answer)
+        assertEquals((((( (log3Mock(value) + log(value, 2)) / (log(value, 5).pow(3)) ) + log(value, 10)) + log(value, 10)) / (log(value, 5) - log(value, 5))),
+            main(value),
+            0.001
+        )
+    }
+
+    @DisplayName("main test log5")
+    @ParameterizedTest
+    @CsvFileSource(resources = ["/results/test-log5-0.1-0.1-1-result.csv"])
+    fun `main test log5`(value: Double, answer: Double){
+        val main = MainFunction().apply { log = log5Mock }
+        Mockito.`when`(log5Mock(value)).thenReturn(answer)
+        assertEquals((((( (log(value, 3) + log(value, 2)) / (log5Mock(value).pow(3)) ) + log(value, 10)) + log(value, 10)) / (log5Mock(value) - log5Mock(value))),
+            main(value),
+            0.001
+        )
+    }
+
+    @DisplayName("main test log10")
+    @ParameterizedTest
+    @CsvFileSource(resources = ["/results/test-log10-0.1-0.1-1-result.csv"])
+    fun `main test log10`(value: Double, answer: Double){
+        val main = MainFunction().apply { log = log10Mock}
+        Mockito.`when`(log10Mock(value)).thenReturn(answer)
+        assertEquals((((( (log(value, 3) + log(value, 2)) / (log(value, 5).pow(3)) ) + log10Mock(value)) + log10Mock(value)) / (log(value, 5) - log(value, 5))),
+            main(value),
+            0.001
+        )
     }
 }
